9d9b220f508ee26dae70d4c03e1d20af82e4c341
therealeddy <eduardo.mfonseca@hotmail.com>
2020-06-18 12:11:19 -0300

    rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+   jwt: {
+      secret: '169006433144f8fbe20d6c00083a6fa4',
+      expiresIn: '1d',
+   },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -30,9 +31,11 @@ class AuthenticateUserService {
          throw new Error('Incorrect email/password combination');
       }
 
-      const token = sign({}, '169006433144f8fbe20d6c00083a6fa4', {
+      const { secret, expiresIn } = authConfig.jwt;
+
+      const token = sign({}, secret, {
          subject: user.id,
-         expiresIn: '1d',
+         expiresIn,
       });
 
       return {
