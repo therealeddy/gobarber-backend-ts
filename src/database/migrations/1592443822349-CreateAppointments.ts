f445f78b470644cb6d785514a47d3c4a8c500868
therealeddy <eduardo.mfonseca@hotmail.com>
2020-06-17 23:34:43 -0300

    repositorio do typeorm

--- src/database/migrations/1592443822349-CreateAppointments.ts
+++ src/database/migrations/1592443822349-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1592443822349
                   type: 'varchar',
                   isPrimary: true,
                   generationStrategy: 'uuid',
+                  default: 'uuid_generate_v4()',
                },
                {
                   name: 'provider',
