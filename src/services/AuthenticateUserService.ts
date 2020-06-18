0f912df44f4438f054c7a97d4f8929801b647369
therealeddy <eduardo.mfonseca@hotmail.com>
2020-06-18 11:13:15 -0300

    gerando token jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +11,7 @@ interface Request {
 
 interface Response {
    user: User;
+   token: string;
 }
 
 class AuthenticateUserService {
@@ -27,8 +30,14 @@ class AuthenticateUserService {
          throw new Error('Incorrect email/password combination');
       }
 
+      const token = sign({}, '169006433144f8fbe20d6c00083a6fa4', {
+         subject: user.id,
+         expiresIn: '1d',
+      });
+
       return {
          user,
+         token,
       };
    }
 }
