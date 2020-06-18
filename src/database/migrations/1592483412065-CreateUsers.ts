1323c8359f4f813b0d7725b4316284c9cdf6c66c
therealeddy <eduardo.mfonseca@hotmail.com>
2020-06-18 10:15:47 -0300

    criacao de registros

--- src/database/migrations/1592483412065-CreateUsers.ts
+++ src/database/migrations/1592483412065-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1592483412065 implements MigrationInterface {
             columns: [
                {
                   name: 'id',
-                  type: 'varchar',
+                  type: 'uuid',
                   isPrimary: true,
                   generationStrategy: 'uuid',
                   default: 'uuid_generate_v4()',
